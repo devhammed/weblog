9421df96998d2531b2aada6c97d448abd8dcc215
Hammed Oyedele <itz.harmid@gmail.com>
2020-07-28 12:16:46 +0100

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new; end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+end
